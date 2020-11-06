71b0008fcc9a69b8e5033d5c73c11db453b3712d
HirushaR <hirusharandunu11@gmail.com>
2020-11-06 19:53:33 +0530

    create album Category

--- components/AlbumCategory/index.tsx
+++ components/AlbumCategory/index.tsx
@@ -1,2 +1,16 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text, View } from 'react-native';
+import {Album} from '../../types';
+
+export  type AlbumCategory = {
+    title: string,
+    albums : [Album]
+}
+const AlbumCategory = (props: AlbumCategory) => (
+    <View>
+        
+
+    </View>
+)
+
+export  default AlbumCategory;
