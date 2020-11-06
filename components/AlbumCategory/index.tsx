5be9fd39c77e9c2a9e19827115099e600a04ca27
HirushaR <hirusharandunu11@gmail.com>
2020-11-06 20:14:17 +0530

    show album list

--- components/AlbumCategory/index.tsx
+++ components/AlbumCategory/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,FlatList } from 'react-native';
 import {Album} from '../../types';
+import AlbumComponent from '../Album';
 import styles  from './style';
 
 export  type AlbumCategory = {
@@ -11,7 +12,14 @@ const AlbumCategory = (props: AlbumCategory) => (
     <View>
         {/* title */}
         <Text style={styles.title}>{props.title}</Text>
+        {/* List of albums */}
+        <FlatList data={props.albums}
+         renderItem={({item}) => <AlbumComponent album={item}/>}
+         keyExtractor= {(item) => item.id }
+         horizontal
+         /> 
 
+        
     </View>
 )
 
